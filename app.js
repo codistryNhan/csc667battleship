a91da95cccfebed95f7b03fa00c28fb92b8f3def
Noraldeno <norald.alejo@gmail.com>
2017-10-19 17:57:44 -0700

    Set up Migration

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+	"development"L{
+		"use_env_variable": "DATABASE_URL",
+		"dialect": "postgres"
+	},
+	"test": {
+		"use_env_variable": "DATABASE_URL",
+		"dialect": "postgres"
+	},
+	"production": {
+		"use_env_variable": "DATABASE_URL",
+		"dialect": "postgres"
+	}
+}
+
 if (process.env.NODE_ENV === 'development'){
 	require("dotenv").config();
 }
