97e3c89525db7415f4e8d82239d50ae7f86d1677
noraldeno <norald.alejo@gmail.com>
2017-10-31 00:26:35 -0700

    Added app

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-require('dotenv').config();
-
-module.exports = {
-	"development"L{
-		"use_env_variable": "DATABASE_URL",
-		"dialect": "postgres"
-	},
-	"test": {
-		"use_env_variable": "DATABASE_URL",
-		"dialect": "postgres"
-	},
-	"production": {
-		"use_env_variable": "DATABASE_URL",
-		"dialect": "postgres"
-	}
-}
-
 if (process.env.NODE_ENV === 'development'){
 	require("dotenv").config();
 }
+
+var express = require('express');
+var path = require('path');
+
+
+var routes = require('./tests');
+
+var router = express.Router();
+var app = express();
+
+app.user(express.static(path.join(__dirname, 'public')));
+
+router.use('/', index);
+
+app.use('/', router);
+
+app.use(function(req, res, next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next){
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	res.status(err.status || 500);
+	res.render('error');
+})
+
+module.exports = app;
---
+++ heroku-cli-v6.14.36-15f8a25-linux-x64/bin/heroku.js
@@ -0,0 +1,6 @@
+const {run} = require('cli-engine')
+const config = {
+  channel: 'stable',
+  version: '6.14.36-15f8a25'
+}
+run({config})
