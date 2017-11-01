11504fa9cf21a67da9ad0694134168fd36e92dbe
noraldeno <norald.alejo@gmail.com>
2017-10-31 18:08:27 -0700

    Did milestone 2

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')(/*options*/);
 
 const connection = pgp(process.env.DATABASE_URL);
 
--- heroku-cli-v6.14.36-15f8a25-linux-x64/bin/heroku.js
+++
@@ -1,6 +0,0 @@
-const {run} = require('cli-engine')
-const config = {
-  channel: 'stable',
-  version: '6.14.36-15f8a25'
-}
-run({config})
