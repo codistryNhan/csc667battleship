d7b2dffd8100d743b6e62a82fe13bc31fffd571b
noraldeno <norald.alejo@gmail.com>
2017-11-04 11:38:22 -0700

    Coded database

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@ const db = require('../db');
 
 /* GET home page. */
 router.get('/', 
-	/*(request, response) => {
+	(request, response) => {
   	db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
 	  .then( _ => db.any(`SELECT * FROM test_table`) )
 	  .then( results => response.json( results ))
 	  .catch( error => {
 	  console.log( error )
 	  response.json({ error })
-	}) */
-	function (req, res, next){
+	})
+	/*function (req, res, next){
 		res.render('index', {
 			title: 'battleship'
 		}
-	)
+	)*/
 });
 
 module.exports = router;
